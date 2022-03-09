ec2f02395b539130cd6ebfd65211e1dbe37dd65c
Kyle Henriksen <khenriksenbootcamp@gmail.com>
2022-03-08 19:16:30 -0800

    updating code; email and github now have working links, and updated card design some more.

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function createHTML() {
     }
     .card {
         box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-        width: 350px;
+        width: 550px;
         height: fit-content;
         border-radius: 1em;
     
